5ee1d970a9ba38ffff747e48b110341c945ab81a
Jeongho Park <pjhpjh0805@naver.com>
2022-05-16 17:03:54 +0900

    네비게이션 바  공통 처리로 변경

--- src/main/java/com/jeongho/shopping/controller/BoardController.java
+++ src/main/java/com/jeongho/shopping/controller/BoardController.java
@@ -45,16 +45,6 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
-//    @PostMapping("/board/writepro")
-//    public String boardWritePro(Board board, Model model) {
-//
-//        boardService.write(board);
-//
-//        model.addAttribute("message", "글 작성이 안료되었습니다.");
-//        model.addAttribute("searchUrl", "/board/list");
-//
-//        return "message";
-//    }
 
     @GetMapping("/board/list")
     public String boardList(Model model) {
@@ -66,49 +56,8 @@ public class BoardController {
         return "boardlist";
     }
 
-//    @GetMapping("/board/view")
-//    public String boardView(Model model, Integer id) {
-//
-//        model.addAttribute("board", boardService.boardView(id));
-//        return "boardview";
-//    }
-
-    @GetMapping("/board/delete")
-    public String boardDelete(Integer id ) {
-
-        boardService.boardDelete(id);
-
-        return "redirect:/board/list";
-    }
 
 
-
-    /*
-
-    @GetMapping("/board/modify/{id}")
-    public String boardModify(@PathVariable("id") Integer id, Model model){
-
-        model.addAttribute("board", boardService.boardView(id));
-
-        return "boardmodify";
-    }
-
-
-    @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model) {
-
-        Board boardtemp = boardService.boardView(id);
-        boardtemp.setTitle(board.getTitle());
-        boardtemp.setContent(board.getContent());
-
-        boardService.write(boardtemp);
-
-        model.addAttribute("message", "글 수정이 안료되었습니다.");
-        model.addAttribute("searchUrl", "/board/list");
-
-        return "message";
-    }
-    */
 
 
 
