1d1e2851e9dbc40e20452364c7d0b9c9fa8f7f60
nadsi96 <nadsi96@naver.com>
2022-05-20 23:49:37 +0900

    WebSecurityConfig antMatchers 지워진거 수정

--- src/main/java/com/jeongho/shopping/config/WebSecurityConfig.java
+++ src/main/java/com/jeongho/shopping/config/WebSecurityConfig.java
@@ -27,7 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/account/register").permitAll()
+                .antMatchers("/test", "/test/**", "/js/**", "/lib/**").permitAll()   // localhost:8080/test/
+                    .antMatchers("/", "/account/register", "/css/**").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
